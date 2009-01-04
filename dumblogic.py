0006fdd96329b65aa4447c825b457e661d622274
Neal Bussett <neal@bussett.com>
2009-01-04 01:31:55 -0800

    Made some more progress.  Still not entirely complete protocol on either side.  Some protocol updates.  Added a few stubbed functions to the client.

--- dumblogic.py
+++ dumblogic.py
@@ -2,32 +2,109 @@
 from sets import Set
 class dumblogic:
 
+# Protocol pieces:
+# x -- Fully supported
+# * -- protocol support (maybe not functional)
+# <- IDENT *
+# -> ID *
+# <- CAPAB *
+# -> CAP *
+# -> STATUS AVAIL|BUSY
+# <- BURN [HASH]
+# -> STATUS [HASH] 0
+# -> STATUS [HASH] %
+# -> STATUS [HASH] 100
+# -> STATUS [HASH] PASS|FAIL
+# <- OPEN
+# -> STATUS OPEN
+# <- WHATIS
+# -> INFO HASH FILE DESC
+
+    VERSION = 1
+
     COM_TERMINATE = '\r\n\r\n'
     COM_DELIM = '\t'
 
     def __init__(self):
-        burn_queue = [] # heapq
-        available_isos = Set() # No prior knowledge of ISOs
-        clients = {} # Collection of clients and their associated data
+        self.burn_queue = [] # Priority queue of what we want to burn
+        self.available_isos = Set() # ISO listing, along with data about ISOs
+        self.conn2client = dict() # Dictionary of connection filenos to IDs (temporary)
+        self.clients = dict() # Dictionary of clients and their associated data (persistent)
         
         # A client is a tuple of the following format:
-        # 
+        # clients[[ID]] = ([type],version,[status],(hashes),misc)
+        # type: COMMAND, DISPLAY, BURNCDR, BURNDVD
+        # hashes: tuple of all hashes (isos) available on that machine (returned from CAPAB)
+        # misc: For burners, what's currently being burned
+        # This is also what we'll expand to include statistics, etc
 
-    def handle_communication(self,(client,inbuf,outbuf)):
+    def handle_communication(self,(conn,inbuf,outbuf)):
         # We know we have a completed command (\r\n\r\n) on inbuf
-        
-        return (client,inbuf,outbuf)
+        message,inbuf = inbuf.split(self.COM_TERMINATE,1)
+        pieces = message.split(self.COM_DELIM) #Could be limited if we need %flow
+        type = ''
+        hashes = ()
+        status = ''
+        misc = ''
+        if pieces[0] == 'ID':
+            if conn.fileno() not in self.conn2client:
+                self.conn2client[conn.fileno()] = pieces[3]
+            if pieces[3] in self.clients: # Old, This doesn't actually do anything yet
+                (type,version,status,hashes,misc) = self.clients[pieces[3]]
+
+            version = pieces[1]
+            type = pieces[2]
+            status = 'NEW'
+
+            self.clients[pieces[3]] = (type,version,status,hashes,misc)
+
+            # This was only for burners, but it makes sense for everyone
+            # since non-burners can push ISOs (eventually)
+            outbuf = outbuf + 'CAPAB' + self.COM_TERMINATE
+
+        if pieces[0] == 'CAP': #reply from CAPAB
+            (type,version,status,hashes,misc) = self.clients[self.conn2client[conn.fileno()]]
+            hashes = pieces[1:]
+            # Check if we have hashes we don't know about
+            # <DUMMY CODE>
+            # Right now: It's all of them
+            for hash in hashes
+                outbuf = outbuf + assemble_message('WHATIS',hash)
+            # </DUMMY CODE>
+            self.clients[self.conn2client[conn.fileno()]] = (type,version,status,hashes,misc)
+
+        if pieces[0] == 'STATUS': # Could be idle, could be burning
+            if is_hash(pieces[1]):
+                # We're burning, or done burning (not idle)
+            else
+                if pieces[1] == 'OPEN':
+                elif pieces[1] == 'AVAIL':
+                elif pieces[1] == 'EMPTY':
+                
+
+
+
+
+        return (conn,inbuf,outbuf)
     
-    def found_new(self,client):
-        # Returns (client,inbuf,outbuf)
+    def found_new(self,conn):
+        # Returns (conn,inbuf,outbuf)
         # We have a new connection, first thing to do is query it
         print "New conn.  Asking for ident"
-        outbuf = 'IDENT' + self.COM_TERMINATE
+        outbuf = 'IDENT' + self.COM_DELIM + 'V' + str(self.VERSION) + self.COM_TERMINATE
         inbuf = ''
-        return (client,inbuf,outbuf)
+        return (conn,inbuf,outbuf)
 
-    def report_missing(self,(client,inbuf,outbuf)):
+    def report_missing(self,(conn,inbuf,outbuf)):
         # Client has gone AWOL, assume its dead.
-        return (client,inbuf,outbuf)
+        del self.conn2client[conn.fileno()]
+        return (conn,inbuf,outbuf)
 
+    def assemble_message(self,parts)
+        return self.COM_DELIM.join(parts) + self.COM_TERMINATE
+    
+    def is_hash(str)
+        if len(str) > 10: # This can be done better, obviously
+            return True
+        return False
 
--- stand-client.py
+++ stand-client.py
@@ -2,8 +2,28 @@
 
 import socket
 
+# Protocol pieces:
+# x -- Fully supported
+# * -- protocol support (maybe not functional)
+# -> IDENT *
+# <- ID *
+# -> CAPAB *
+# <- CAP *
+# <- STATUS AVAIL|BUSY
+# -> BURN [HASH] *
+# <- STATUS [HASH] 0 *
+# <- STATUS [HASH] %
+# <- STATUS [HASH] 100
+# <- STATUS [HASH] PASS|FAIL
+# -> OPEN *
+# <- STATUS OPEN *
+# -> WHATIS *
+# <- INFO HASH FILE DESC *
+
 PORT = 1337
 HOST = 'localhost'
+
+VERSION = 1
 
 COM_TERMINATE = '\r\n\r\n'
 COM_DELIM = '\t'
@@ -24,7 +44,27 @@ def send(str):
         except socket.error: # We only want to catch SIGPIPE (server disconnect)
             pass
 
+def get_hashes():
+    # Obviously this is a stub
+    # Returns a tuple of hashes
+    return ('38e3f4d0774a143bd24f1f2e42e80d63','bbd21ded02c06b41c59485266833937a','b78ef719e3361e726b89bab78c526ad0','c69e34e92d5402d1b87e6babc739f774')
 
+def drive_type():
+    # Returns the type of the drive (assumed that DVD drives can burn CDs)
+    return 'CDR'
+
+def disc_type():
+    # Returns the type (CDR or DVD) of the disc in the drive
+    return 'CDR'
+
+def image_info(hash):
+    # This is just hard-coded right now, obviously it needs to look up appropriate stuff
+    # Returns a tuple of the image hash, filename, and the description.  If we want to support tabs in the description,
+    # we need to modify the server code slightly (for an empty line in the description, use '\r\n \r\n')
+    return ('38e3f4d0774a143bd24f1f2e42e80d63','ubuntu-8.04.1-desktop-i386.iso',"Ubuntu 8.04 (Hardy Heron) 32bit Desktop Image\r\n \r\nThis image is for the most common computers out there.  If you're in doubt, get this image.")
+
+def tray_open():
+    return
 
 # create a socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -57,7 +97,6 @@ while(1):
         if len(server.recv(1024,socket.MSG_PEEK)) == 0:
             server.close() # Bad connection, We likely want to reconnect
             continue
-
         # otherwise: do stuff with inbuf
         inbuf = inbuf + server.recv(1024)
 
@@ -69,9 +108,34 @@ while(1):
     if inbuf.find(COM_TERMINATE) > -1:
         # We've got a full command
         message,inbuf = inbuf.split(COM_TERMINATE,1) # We only want one message at a time
-        if message == 'IDENT':
-            print 'IDENT recieved'
-            send(('ID','BURN',unique_ident)) # We're a burner right now
+        pieces = message.split(COM_DELIM)
+        if pieces[0] == 'IDENT':
+            print 'RX: IDENT, V',pieces[1]
+            send(('ID','V' + str(VERSION),'BURN'+ drive_type(),unique_ident)) # We're a CD burner right now
+        if pieces[0] == 'CAPAB':
+            print 'RX: CAPAB'
+            send(('CAP','BURN' + drive_type(),COM_DELIM.join(get_hashes()))) # We could re-split hashes; that's silly
+        if pieces[0] == 'WHATIS':
+            send('INFO',COM_DELIM.join(image_info(pieces[1]))) # We could re-split info, but its not required
+        if pieces[0] == 'OPEN':
+            # Open the tray
+            open_tray()
+            # (sending stuff will happen as a response to the tray opening)
+            pass
+        if pieces[0] == 'BURN':
+            if pieces[1] not in get_hashes():
+                # Error
+                pass
+            else:
+                # Start Burn
+                send(('STATUS',pieces[1],'0')) # Burn starts at 0
+                #Timer starts
+
+    # That's everything that the server will send us (except for file transfer stuff)
+    # Everything else is generated based on stuff happening locally
+    if get_tray_status == 'open':
+        send(('STATUS','OPEN'))
+    
 
 
 
