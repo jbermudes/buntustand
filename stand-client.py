b6097425ed82c649ef7b59f9ba2d8df3a1160d47
Neal Bussett <neal@bussett.com>
2009-01-18 16:05:38 -0800

    Client Display works.  Open/Avail/Empty may need cosmetic sprucing.

--- stand-client.py
+++ stand-client.py
@@ -23,6 +23,8 @@ COLOR_U = 1
 COLOR_K = 2
 COLOR_E = 3
 COLOR_X = 4
+COLOR_PASS = 5
+COLOR_FAIL = 6
 
 tabnames = ("Order","Packages","Clients","Queue","")
 tabs = []
@@ -46,6 +48,19 @@ def get_ident(): # Return our client ID
     # Persistence only matters for burners, so we can use 
     # pseudo-random for command/display
     return '033312ebed6b1e5c5a691fd6e24f7535'
+
+def is_hash(str):
+    if len(str) > 10: # This can be done better, obviously
+        return True
+    return False
+
+# CD info stuff
+def get_hash_info(hash): # Dummy
+    info = [('Ubuntu','8.04.1','AMD64','Desktop'),('Kubuntu','8.10','i386','Alternate'),
+            ('Xubuntu','6.06.2','i386','Server'),('Edubuntu','8.04.1','AMD64','Alternate')]
+    index = int(hash,16) % len(info)
+    return info[index]
+
 
 
 ## Curses Functions
@@ -173,25 +188,113 @@ def output_Q_item(scr,index,item,pri):
 def update_display_clients(scr):
     # Loop through local copy of clients and call output_client
     for i in range(0,6):
-        output_client(scr,i,' ')
-
+        output_client_clear(scr,i)
+    for i in range(0,len(clients)):
+        output_client(scr,i,clients[i])
 
 def output_client(scr,index,client):
-    scr.addstr(1,index*11+2,'test')
+#   logging.debug('output: ' + client[3])
+    if client[3].count('\t') < 1: # Things without a tab, those things are status
+        hash = 'x'
+        status = client[3]
+    else:
+        hash,status = client[3].split(COM_DELIM) # hash,status
+    if is_hash(hash):
+        item = get_hash_info(hash) 
+        if item[0] == 'Ubuntu':
+           attr = curses.color_pair(COLOR_U)
+        elif item[0] == 'Kubuntu':
+           attr = curses.color_pair(COLOR_K) | curses.A_BOLD
+        elif item[0] == 'Edubuntu':
+           attr = curses.color_pair(COLOR_E) | curses.A_BOLD
+        elif item[0] == 'Xubuntu':
+           attr = curses.color_pair(COLOR_X) | curses.A_BOLD
+    
+        if item[1][0:4] == '6.06':
+            ver = 'D'
+        elif item[1] == '6.10':
+            ver = 'E'
+        elif item[1] == '7.04':
+            ver = 'F'
+        elif item[1] == '7.10':
+            ver = 'G'
+        elif item[1][0:4] == '8.04':
+            ver = 'H'
+        elif item[1] == '8.10':
+            ver = 'I'
+        elif item[1] == '9.04':
+            ver = 'J'
 
-def get_queue():
+        scr.addstr(1,index*11+1,item[0][0:1],attr)
+        scr.addstr(1,index*11+3,ver)
+        scr.addstr(1,index*11+5,item[2][0:1])
+        scr.addstr(1,index*11+7,item[3][0:1])
+
+        #Now check Status
+        attr = 0
+        if status == '0':
+            attr |= 0
+            prog = '0%'
+        elif status == '25':
+            attr |= curses.A_REVERSE
+            prog = '25%'
+        elif status == '50':
+            attr |= curses.A_REVERSE
+            prog = '50% '
+        elif status == '75':
+            attr |= curses.A_REVERSE
+            prog = '75%   '
+        elif status == '100': #Time won't come through (stopped at server)
+            attr |= curses.A_REVERSE
+            prog = ' VERIFY '
+        elif status == 'PASS':
+            attr |= curses.color_pair(COLOR_PASS) | curses.A_BOLD
+            prog = '* PASS *'
+        elif status == 'FAIL':
+            attr |= curses.color_pair(COLOR_FAIL) | curses.A_BOLD
+            prog = '! FAIL !'
+        scr.addstr(2,index*11+1,prog,attr)
+
+    else: # not a hash, must be AVAIL|EMPTY|OPEN # These will be status, hash will be 'x'
+        # Itd be nice to not have to swap avail|empty and CDR|DVD.  Swap protocols to make that happen
+        attr = 0 # Need some way to *ask* for a DVD instead of a CD
+        if status == 'OPEN':
+            attr |= 0
+            stat = 'OPEN'
+        elif status == 'AVAIL':
+            attr |= 0
+            stat = '* AVAIL '
+        elif status == 'EMPTY':
+            attr |= 0
+            stat = '* EMPTY '
+
+        scr.addstr(2,index*11+1,stat,attr)
+
+    scr.addnstr(0,index*11+1,client[1],9) # All clients have names
+
+
+def output_client_clear(scr,index):
+    for i in range(0,3):
+        scr.addstr(i,index*11,'          ')
+
+def get_queue(): # Dummy Queue Population
     queue.append(('12b4','5'))
     queue.append(('1231','7'))
     queue.append(('1233','10'))
     queue.append(('1232','15'))
     queue.append(('b0cf','1'))
 
-# CD info stuff
-def get_hash_info(hash): # Dummy
-    info = [('Ubuntu','8.04.1','AMD64','Desktop'),('Kubuntu','8.10','i386','Alternate'),
-            ('Xubuntu','6.06.2','i386','Server'),('Edubuntu','8.04.1','AMD64','Alternate')]
-    index = int(hash,16) % len(info)
-    return info[index]
+def get_clients(): # Dummy Client Population
+    # ID, Name, Type, Status # image is included in status... is type as well?
+    clients.append(('033312ebed6b1e5c5a691fd6e24f7532','Client0','CDR','1231231231231231231234\t0'))
+    #clients.append(('033312ebed6b1e5c5a691fd6e24f7532','Client0','CDR','AVAIL'))
+    clients.append(('033312ebed6b1e5c5a691fd6e24f7539','Client12345678','CDR','12312312312312312311237\t25'))
+    #clients.append(('033312ebed6b1e5c5a691fd6e24f7539','Client12345678','CDR','EMPTY'))
+    clients.append(('033312ebed6b1e5c5a691fd6e24f7531','Client2','CDR','1231231231231231231231\t50'))
+    #clients.append(('033312ebed6b1e5c5a691fd6e24f7531','Client2','CDR','OPEN'))
+    clients.append(('033312ebed6b1e5c5a691fd6e24f7536','Client3','CDR','1231231231231231232\t100'))
+    clients.append(('033312ebed6b1e5c5a691fd6e24f7532','Client4','CDR','1231231231231231236\tPASS'))
+    clients.append(('033312ebed6b1e5c5a691fd6e24f7535','Client5','CDR','1231231231231231230\tFAIL'))
 
 ## Main stuffs
 
@@ -201,6 +304,8 @@ def main(stdscr):
     curses.init_pair(2,curses.COLOR_BLUE,curses.COLOR_BLACK) # Kubuntu Blue on Black
     curses.init_pair(3,curses.COLOR_RED,curses.COLOR_BLACK) # Edubuntu
     curses.init_pair(4,curses.COLOR_CYAN,curses.COLOR_BLACK) # Xubuntu
+    curses.init_pair(5,curses.COLOR_BLACK,curses.COLOR_GREEN) # Pass
+    curses.init_pair(6,curses.COLOR_BLACK,curses.COLOR_RED) # Fail (This could be reverse edubuntu)
 
     stdscr.bkgd('*', curses.A_REVERSE )
     win_sidebar = curses.newwin(21,15,0,0)
@@ -242,6 +347,7 @@ def main(stdscr):
 
     # Dummy stuff
     get_queue()
+    get_clients()
 
     active_tab = 0
     stale_queue = True
